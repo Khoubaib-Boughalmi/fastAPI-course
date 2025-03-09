c7797ed39787a77aefa8c728c24620f57d750fd8
khoubaib boughalmi <khoubaib.kb99@gmail.com>
2025-03-10 00:16:57 +0100

    permission decorator with jwt role

--- miscellaneous/permission_decorator.py
+++ miscellaneous/permission_decorator.py
@@ -1,5 +1,8 @@
-from fastapi import Depends, FastAPI, HTTPException
+from typing import Optional
+from fastapi import Depends, FastAPI, HTTPException, Header, Request
 from functools import wraps
+
+import jwt
 app = FastAPI()
 
 
@@ -16,13 +19,37 @@ def require_permission(role):
 		return wrapper
 	return decorator
 
-def extract_user_permission():
-	"""
-		This function can be used to extract user role from jwt or session
- 	"""
-	return "supervisor"
+SECRET_KEY = "mySecretKey"
 
+def jwt_decode_token(token: str) -> dict:
+	print("toooken", token)
+	try:
+		decoded = jwt.decode(token, key=SECRET_KEY, algorithms=["HS256"])
+		role = decoded.get("role", "guest")
+		return role
+	except jwt.ExpiredSignatureError:
+		raise HTTPException(status_code=403, detail="Token has expired.")
+	except jwt.InvalidTokenError:
+		raise HTTPException(status_code=403, detail="Invalid token.")     
+	except Exception as e:
+		raise HTTPException(status_code=403, detail=e)     
+		
+def extract_user_permission(authorization: Optional[str] = Header()) -> str:
+	"""
+		This function can be used to extract user role from jwt
+ 	"""
+	if authorization is None:
+		raise HTTPException(status_code=401, detail="Authorization header missing")
+	token = authorization.replace("Bearer ", "")
+	try:
+		decoded_token = jwt_decode_token(token)
+		return decoded_token
+	except:
+		return "guest"
+	
+ 
 @app.get("/admin")
 @require_permission("admin")
 async def get_admin_information(user_role: str = Depends(extract_user_permission)):     
 	return {"Information": "This is a secret information only accessible to admins", "role": user_role}
+
